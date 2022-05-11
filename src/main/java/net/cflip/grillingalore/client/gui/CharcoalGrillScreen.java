0a7be85dd3674f43230ec9793cdb8182eacea711
cflip <cflip@cflip.net>
2022-05-10 19:42:23 -0600

    Mark CharcoalGrillScreen as client-only

--- src/main/java/net/cflip/grillingalore/client/gui/CharcoalGrillScreen.java
+++ src/main/java/net/cflip/grillingalore/client/gui/CharcoalGrillScreen.java
@@ -2,10 +2,13 @@ package net.cflip.grillingalore.client.gui;
 
 import net.cflip.grillingalore.GrillinGalore;
 import net.cflip.grillingalore.screen.CharcoalGrillScreenHandler;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+@Environment(EnvType.CLIENT)
 public class CharcoalGrillScreen extends AbstractGrillScreen<CharcoalGrillScreenHandler> {
 	private static final Identifier TEXTURE = new Identifier(GrillinGalore.MODID, "textures/gui/container/charcoal_grill.png");
 
